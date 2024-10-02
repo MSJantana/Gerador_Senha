4a80faa336aa3fe91b2c8d4757f37a6f9e5fbff6
unknown <Administrador@USB-ASR-AUXMIDIA.adventistas.local>
2024-10-02 16:16:27 -0300

    Removido Packetbeat da Configuração .sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,8 @@
 filebeat modules enable apache
 filebeat setup
 
-# Ativa e configura os módulos do Packetbeat
-packetbeat modules enable apache
-packetbeat setup
-
 # Inicializa o Filebeat em segundo plano
 filebeat -e &
-
-# Inicializa o Packetbeat em segundo plano
-packetbeat -e &
 
 # Inicia o Apache (ou outro serviço principal do container)
 exec "$@"
