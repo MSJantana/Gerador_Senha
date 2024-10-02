ed8087c8207d2d8f80ca95463ca45a2c88ad2544
unknown <Administrador@USB-ASR-AUXMIDIA.adventistas.local>
2024-10-02 13:09:09 -0300

    Atualização do Filebeat e Packetbeat-Modficaição no Atributo do .sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # Script de entrada (entrypoint) para inicialização do container
 
+filebeat modules enable apache
+filebeat modules enable http
+filebeat modules setup
+
+packetbeat modules enable apache
+packetbeat modules enable http
+packetbeat modules setup
+
 # Inicializa o Filebeat e Packetbeat em segundo plano
 service filebeat start
 service packetbeat start
